4d6d665e7d8f7dd489b5d7c38bebee4d4d48bada
Bob Dijks <rhcdijks@gmail.com>
2021-07-19 20:00:36 -0500

    added validations to Pokemon model

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,2 +1,6 @@
 class Pokemon < ApplicationRecord
+  validates :name, presence: true
+  validates :type, presence: true
+  validates :ability, presence: true
+  validates :weight, presence: true
 end
